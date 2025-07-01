29b762962d1d9179e13e6f70df6e7c4a11bb3aea
Cristian Bourceanu <v.c.bourceanu@sms.ed.ac.uk>
2025-07-01 17:01:02 +0100

    Filter our null TDC codes

--- src/processing.jl
+++ src/processing.jl
@@ -124,12 +124,12 @@ function filter_code(tdc_pixels::Union{Array{UInt8}, Array{UInt16}}; decode_mode
   nan_boxed_pixels =
     if decode_mode == Decoded
         if tdc_pixels isa Array{UInt16}
-            map(x-> if(x==0xffc) missing else Float32(x) end, tdc_pixels)
+            map(x-> if(x==0xffc || x==0x000) missing else Float32(x) end, tdc_pixels)
         else
-            map(x-> if(x==0xfc) missing else Float32(x) end, tdc_pixels)
+            map(x-> if(x==0xfc || x==0x00) missing else Float32(x) end, tdc_pixels)
         end
     else
-        map(x-> if(x==0x04) missing else Float32(x) end, tdc_pixels)
+        map(x-> if(x==0x000) missing else Float32(x) end, tdc_pixels)
     end
   nan_boxed_pixels
 end
